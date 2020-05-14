d497f201fd35dce880dda771cb1f160975d2777c
Anton Krasovsky <anton.krasovsky@gmail.com>
2020-05-14 22:49:28 +0100

    Add copyright notice to bundler files

--- src/main/java/com/xliic/oas/bundler/Serializer.java
+++ src/main/java/com/xliic/oas/bundler/Serializer.java
@@ -1,3 +1,8 @@
+/*
+ Copyright (c) 42Crunch Ltd. All rights reserved.
+ Licensed under the GNU Affero General Public License version 3. See LICENSE.txt in the project root for license information.
+*/
+
 package com.xliic.oas.bundler;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
