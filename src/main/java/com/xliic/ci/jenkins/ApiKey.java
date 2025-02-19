dac25ee50d5b1806f1f864c5add27593695c31b7
gounthar <gounthar@gmail.com>
2025-02-19 11:17:15 +0100

    Refactor ApiKey.java for improved formatting and consistency

--- src/main/java/com/xliic/ci/jenkins/ApiKey.java
+++ src/main/java/com/xliic/ci/jenkins/ApiKey.java
@@ -5,19 +5,18 @@
 
 package com.xliic.ci.jenkins;
 
-import javax.annotation.Nonnull;
-
 import com.cloudbees.plugins.credentials.CredentialsNameProvider;
 import com.cloudbees.plugins.credentials.NameWith;
 import com.cloudbees.plugins.credentials.common.StandardCredentials;
-
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Util;
 import hudson.util.Secret;
+import javax.annotation.Nonnull;
 
 @NameWith(ApiKey.NameProvider.class)
 public interface ApiKey extends StandardCredentials {
-    static final String UUID_PATTERN = "(api_)?[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}";
+    static final String UUID_PATTERN =
+            "(api_)?[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}";
 
     @Nonnull
     Secret getApiKey();
@@ -30,5 +29,4 @@ public interface ApiKey extends StandardCredentials {
             return description != null ? description : Messages.ApiKeyImpl_api_key();
         }
     }
-
 }
